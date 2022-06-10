154d8745f19b9e66d7f561969f81dd540577587c
kunalkarwarkar <karwarkarkunal@gmail.com>
2022-06-10 17:47:36 +0530

    checkpoint 2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './common/Header/Header.js'
+
+function App() {
+  return (
+    <div className="App"></div>
+    <Header />
+    </div>
+  );
+}
+export default App;
